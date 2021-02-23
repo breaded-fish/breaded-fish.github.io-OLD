2ca15a3968477c309afb8d2fecc64d13905859fd
Oscar Fung <47403741+breaded-fish@users.noreply.github.com>
2021-02-22 21:42:06 -0500

    Add files via upload

--- resource/javascript-files/canvas_chart.js
+++ resource/javascript-files/canvas_chart.js
@@ -29,7 +29,6 @@ function drawrect(x, y, length, width, color){
   ctx_chart.fillStyle = color;
   ctx_chart.fillRect(x, y, length, width);
 }
-
 /*
 function drawtext:
     draws text
@@ -42,17 +41,16 @@ function drawtext:
 function drawtext(text, font, color, x, y){
   ctx_chart.font = font;
   ctx_chart.fillStyle= color;
+  ctx_chart.textAlign = "center";
   ctx_chart.fillText(text, x, y);
-
+  
 }
-
 /*
-Special function drawgrid
+  Special function drawgrid
     -draws a grid for ease of mapping
     -circle = 100 units;
-    -currently not working;
+
 */
-drawgrid(false);//I created a boolean for the later uses, maybe let the user turn on the grid?
 function drawgrid(boolean){
   var w = (canvas_chart.width/100);
   var h = (canvas_chart.height/100);
@@ -71,6 +69,8 @@ function drawgrid(boolean){
   }
 }
 
+drawgrid(false);//I created a boolean for the later uses, maybe let the user turn on the grid?
+
 var fruits = {
 
   "name":["Apple","Orange", "Banana", "Kiwi", "Blueberry", "Grapes"],
@@ -82,13 +82,6 @@ var fruits = {
 for(var f = 0; f < canvas_chart.height/100; f++)
 {
   drawrect(f*100, 600, 100, (fruits.quantity[f])*-30, fruits.color[f]);
-  drawtext(fruits.name[f],'21px Century Gothic','black',(f*100),550);
-  drawtext(fruits.quantity[f],'21px Century Gothic','black',(f*100),575);
+  drawtext(fruits.name[f],'18px Century Gothic','black',(f*100)+50,550);
+  drawtext(fruits.quantity[f],'20px Century Gothic','black',((f*100) + 50),575);
 }
-
-/*drawrect(0, 600, 100, -600,'red');
-drawrect(100, 600, 100, -300,'orange');
-drawrect(200, 600, 100, -450,'yellow');
-drawrect(300, 600, 100, -90,'green');
-drawrect(400, 600, 100, -150,'blue');
-drawrect(500, 600, 100, -240,'purple');*/
