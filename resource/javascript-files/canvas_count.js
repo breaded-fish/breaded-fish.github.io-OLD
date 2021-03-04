4c0f9fc974ced8960966bf7c9941077f0e606692
Oscar Fung <47403741+breaded-fish@users.noreply.github.com>
2021-03-03 23:56:14 -0500

    Add files via upload

--- resource/javascript-files/canvas_count.js
+++ resource/javascript-files/canvas_count.js
@@ -1,233 +1,133 @@
-const canvas_count = document.getElementById('canvas_count');
-const ctx_count = canvas_count.getContext('2d');
+
 
 //sprite sheet array
 var spritesheet = 
 {
 	"frames": [
 		{
-			"filename": "0.png",
 			"frame": {
 				"x": 1,
 				"y": 1,
 				"w": 83,
 				"h": 144
 			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 83,
-				"h": 144
-			},
 			"sourceSize": {
 				"w": 83,
 				"h": 144
 			}
 		},
 		{
-			"filename": "1.png",
 			"frame": {
 				"x": 86,
 				"y": 1,
 				"w": 55,
 				"h": 141
 			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 55,
-				"h": 141
-			},
 			"sourceSize": {
 				"w": 55,
 				"h": 141
 			}
 		},
 		{
-			"filename": "2.png",
 			"frame": {
 				"x": 143,
 				"y": 1,
 				"w": 81,
 				"h": 142
 			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 81,
-				"h": 142
-			},
 			"sourceSize": {
 				"w": 81,
 				"h": 142
 			}
 		},
 		{
-			"filename": "3.png",
 			"frame": {
 				"x": 226,
 				"y": 1,
 				"w": 77,
 				"h": 143
 			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 77,
-				"h": 143
-			},
 			"sourceSize": {
 				"w": 77,
 				"h": 143
 			}
 		},
 		{
-			"filename": "4.png",
 			"frame": {
 				"x": 305,
 				"y": 1,
 				"w": 82,
 				"h": 141
 			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 82,
-				"h": 141
-			},
 			"sourceSize": {
 				"w": 82,
 				"h": 141
 			}
 		},
 		{
-			"filename": "5.png",
 			"frame": {
 				"x": 389,
 				"y": 1,
 				"w": 79,
 				"h": 143
 			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 79,
-				"h": 143
-			},
 			"sourceSize": {
 				"w": 79,
 				"h": 143
 			}
 		},
 		{
-			"filename": "6.png",
 			"frame": {
 				"x": 470,
 				"y": 1,
 				"w": 80,
 				"h": 141
 			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 80,
-				"h": 141
-			},
 			"sourceSize": {
 				"w": 80,
 				"h": 141
 			}
 		},
 		{
-			"filename": "7.png",
 			"frame": {
 				"x": 552,
 				"y": 1,
 				"w": 80,
 				"h": 140
 			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 80,
-				"h": 140
-			},
 			"sourceSize": {
 				"w": 80,
 				"h": 140
 			}
 		},
 		{
-			"filename": "8.png",
 			"frame": {
 				"x": 634,
 				"y": 1,
 				"w": 80,
 				"h": 142
 			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 80,
-				"h": 142
-			},
 			"sourceSize": {
 				"w": 80,
 				"h": 142
 			}
 		},
 		{
-			"filename": "9.png",
 			"frame": {
 				"x": 716,
 				"y": 1,
 				"w": 80,
 				"h": 144
 			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 80,
-				"h": 144
-			},
 			"sourceSize": {
 				"w": 80,
 				"h": 144
 			}
 		},
 		{
-			"filename": "blank.png",
 			"frame": {
 				"x": 798,
 				"y": 1,
-				"w": 83,
-				"h": 144
-			},
-			"rotated": false,
-			"trimmed": false,
-			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
 				"w": 83,
 				"h": 144
 			},
@@ -236,23 +136,13 @@ var spritesheet =
 				"h": 144
 			}
 		}
-	],
-	"meta": {
-		"app": "http://www.codeandweb.com/texturepacker",
-		"version": "1.0",
-		"image": "spritesheet.png",
-		"format": "RGBA8888",
-		"size": {
-			"w": 882,
-			"h": 146
-		},
-		"scale": "1"
-	}
+	]
 }
 let img = new Image();
 img.src = './resource/spritesheet.png';
 
-
+const canvas_count = document.getElementById('canvas_count');
+const ctx_count = canvas_count.getContext('2d');
 
 /*Function drawFrame
 	Reads from a JSON array and finds specific x and y coordinates on a sprite sheet.
