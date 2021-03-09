0986e6cd45f3ae8e761a08e3fac7d51718ffe38e
Oscar Fung <47403741+breaded-fish@users.noreply.github.com>
2021-03-08 19:15:53 -0500

    Add files via upload

--- resource/javascript-files/lab_notes_input.js
+++ resource/javascript-files/lab_notes_input.js
@@ -5,18 +5,25 @@ var clear = document.getElementById("clearButton");
 
 //On input, the Title and Notes are saved seperately into localstorage
 notes.oninput = function(){
-  localStorage.setItem("title",noteTitle.value);
-  localStorage.setItem("notes",notes.value);
+  setValue();
 }
 
 //When user comes back, onload it will check if there is anything in localstorage, if not then it will just be black
 window.onload = function(){
-  document.getElementById("noteTitle").value = localStorage.getItem("title");
-  document.getElementById("notes").value = localStorage.getItem("notes");
+  noteTitle.value = localStorage.getItem("title");
+  notes.value = localStorage.getItem("notes");
 }
 
 //Whether or not it has text, button will clear the text in the boxes and since it counts as input it will be saved.
 clear.onclick = function(){
-  document.getElementById("noteTitle").value = "";
-  document.getElementById("notes").value = "";
+  noteTitle.value = "";
+  notes.value = "";
+  setValue();
+}
+
+/*function setValue()
+Since it is being called multiple times, why not just make it a function?*/
+function setValue(){
+  localStorage.setItem("title",noteTitle.value);
+  localStorage.setItem("notes",notes.value);
 }
